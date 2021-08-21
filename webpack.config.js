d7f8045ce129c8f8ffaf85d6a64ac304bf24b50b
EricaBabb <ecamilebabb@gmail.com>
2021-08-21 15:50:53 -0400

    PWA app

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const webpack = require("webpack");
 
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 //Main config object
 //We'll write options within this object that tell webpack what to do
@@ -56,21 +57,33 @@ const config = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-          })
-        // new WebpackPwaManifest({
-        //   name: "Food Event",
-        //   short_name: "Foodies",
-        //   description: "An app that allows you to view upcoming food events.",
-        //   background_color: "#01579b",
-        //   theme_color: "#ffffff",
-        //   fingerprints: false,
-        //   inject: false,
-        //   icons: [{
-        //     src: path.resolve("assets/img/icons/icon-512x512.png"),
-        //     sizes: [96, 128, 192, 256, 384, 512],
-        //     destination: path.join("assets", "icons")
-        //   }]
-        // })
+          }),
+          //"new" invokes a constructor function
+        new WebpackPwaManifest({
+
+          //the name will show up next to the app's icon on desktop devices
+          name: "Food Event",
+
+          //short_name is the name that will appear on a user's home screen when the app has been downloaded
+          short_name: "Foodies",
+          description: "An app that allows you to view upcoming food events.",
+
+          //specify the homepage for the PWA relative to the location of the manifest file
+          start_url: "../index.html",
+
+          // If the app launches before the browser is loaded, the background color and theme will appear
+          background_color: "#01579b",
+
+          //The theme_color sets the color of the tool bar
+          theme_color: "#ffffff",
+          fingerprints: false,
+          inject: false,
+          icons: [{
+            src: path.resolve("assets/img/icons/icon-512x512.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
+          }]
+        })
       ],
     mode: 'development'  
 };
@@ -78,6 +91,10 @@ const config = {
 module.exports = config;
 
 //analyzerMode: "disable" to temporarily stop the reporting and automatic opening of this report in the browser.
+
+//Fingerprints tell webpack whether or not it should generate unique fingerprints so that each time a new manifest is generated, it looks like this: manifest.lhge325d.json
+
+//inject property determines whether the link to the manifest.json is added to the HTML. Because we are not using fingerprints, we can also set inject to be false. We will hardcode the path to the manifest.json instead, just like we would in an application without webpack.
 
 //mode: development will have a hot-reloading of webpack and debugging features
 //mode: production will build source files into minifiy files
